b245c9bb60dc6599929d062df26a5e4204a1f821
野猪大大 <541639809@qq.com>
2019-08-28 08:16:43 +0800

    commit

--- V1/component.go
+++ V1/component.go
@@ -1,3 +1,26 @@
 package main
 
+import "sync"
 
+const (
+	COMPONENT_STATE_NONE = iota
+	COMPONENT_STATE_NORMAL
+	COMPONENT_STATE_CLOSED
+)
+
+type ComponentState = int
+
+type IComponent interface {
+
+}
+
+type Reference struct {
+	Sys ISystem
+	Index *int
+}
+
+type ComponentBase struct {
+	sync.RWMutex
+	owner IEntity
+	state ComponentState
+}
